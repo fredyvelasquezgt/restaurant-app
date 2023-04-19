5ab5fcec7dea1a21cb96963c73eca4672c0d0a9e
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-18 23:42:23 -0600

    editing page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
-     
+     <h1>Hello my friends</h1>
     </main>
   )
 }
